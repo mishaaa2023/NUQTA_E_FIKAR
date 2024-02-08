ed165f6eab54e7c30fc37764eeb6fcc25767a5ff
mishaaa2023 <148787583+mishaaa2023@users.noreply.github.com>
2024-02-08 13:48:44 +0500

    uploaded all files

---
+++ script.js
@@ -0,0 +1,117 @@
+function loadinganimation() {
+  gsap.from("#home h1", {
+    y: 100,
+    opacity: 0,
+    delay: 0.5,
+    duration: 0.9,
+    stagger: 0.3,
+  });
+  gsap.from("#home img", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+  gsap.from("#home p", {
+    scale: 0.9,
+    opacity: 0,
+    delay: 1.3,
+    duration: 0.5,
+  });
+}
+loadinganimation();
+
+function circleAnimation() {
+  var videocon = document.querySelector("#about");
+  var playbtn = document.querySelector("#play1");
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 70,
+      top: dets.y - 80,
+    });
+  });
+}
+circleAnimation();
+
+function circleAnimation1() {
+  var videocon = document.querySelector("#mission");
+  var playbtn = document.querySelector("#play2");
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 70,
+      top: dets.y - 80,
+    });
+  });
+}
+circleAnimation1();
+
+function circleAnimation2() {
+  var videocon = document.querySelector("#dealings");
+  var playbtn = document.querySelector("#play3");
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 70,
+      top: dets.y - 80,
+    });
+  });
+}
+circleAnimation2();
+function circleAnimation3() {
+  var videocon = document.querySelector("#contact");
+  var playbtn = document.querySelector("#play4");
+  videocon.addEventListener("mouseenter", function () {
+    gsap.to(playbtn, {
+      scale: 1,
+      opacity: 1,
+    });
+  });
+  videocon.addEventListener("mouseleave", function () {
+    gsap.to(playbtn, {
+      scale: 0,
+      opacity: 0,
+    });
+  });
+  document.addEventListener("mousemove", function (dets) {
+    gsap.to(playbtn, {
+      left: dets.x - 70,
+      top: dets.y - 80,
+    });
+  });
+}
+circleAnimation3();
